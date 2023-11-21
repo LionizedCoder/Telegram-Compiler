7891c8e32618ffcc4651f447ffaaba11f23c9b13
rocco.malvaso <malvasorocco3@gmail.com>
2023-11-21 23:11:55 +0100

    Add test for compiler function, try to print the payload

--- callbacks/compile.py
+++ callbacks/compile.py
@@ -6,19 +6,16 @@ from utils.constant import payload
 def compile(update: Update, context: CallbackContext) -> int:
     user_input = ''.join(context.args)
     if user_input != "":
-        text = "Stai compilando questo codice:\n" + user_input
-        update.message.reply_html(text)
-        result = compiler(update.message.text)
-        update.message.reply_html(result['asm'] + result['code'])
+        text = "Stai compilando questo codice:\n" + f"```rust\n{user_input}```"
+        update.message.reply_text(text, parse_mode='Markdown')
+        payload['code'] = user_input
+        r = requests.post(f"https://godbolt.org/api/compiler/r1730/compile",  json=payload,
+        headers={'Accept': 'application/json'})
+        update.message.reply_html("<b>In costruzione..</b>")
+        # result = r.text
+        # update.message.reply_html(result)
     else:
         update.message.reply_html("Non hai inserito nessun codice da compilare")
 
-
-
-def compiler(code : str):
-    payload['code'] = code
-    r = requests.post(f"https://godbolt.org/api/compiler/r1730/compile",  json=payload,
-        headers={'Accept': 'application/json'})
-    reply = r.json()
 
 
