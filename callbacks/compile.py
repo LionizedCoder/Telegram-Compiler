ccfd086edb4842f8d55e86570e767e22f409662d
rocco.malvaso <malvasorocco3@gmail.com>
2023-11-21 22:34:05 +0100

    Add response on compile
    Add MessageWriter class with methods and Output Kind

--- callbacks/compile.py
+++ callbacks/compile.py
@@ -2,17 +2,25 @@ import requests
 from telegram import Update
 from telegram.ext import CallbackContext
 from utils.constant import payload
-from utils.utils import CompileResult, lines_output
+from utils.utils import CompileResult, lines_output, OutputKind
+
+MESSAGE_LIMIT = 1
+
 
 def compile(update: Update, context: CallbackContext) -> int:
-    user_input = ' '.join(context.args)
-    text = "Stai compilando questo codice:\n" + user_input
-    update.message.reply_html(text)
-    result = compiler(update.message.text)
-    update.message.reply_html(result)
+    user_input = ''.join(context.args)
+    if user_input != "":
+        text = "Stai compilando questo codice:\n" + user_input
+        update.message.reply_html(text)
+        result = compiler(update.message.text)
+        for msg in result.to_messages(OutputKind.ALL)[:MESSAGE_LIMIT]:
+            update.message.reply_html(msg)
+    else:
+        update.message.reply_html("Non hai inserito nessun codice da compilare")
 
 
-def compiler(code : str) -> str:
+
+def compiler(code : str) -> CompileResult:
     payload['code'] = code
     r = requests.post(f"https://godbolt.org/api/compiler/r1730/compile",  json=payload,
         headers={'Accept': 'application/json'})
