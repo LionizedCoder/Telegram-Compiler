2c728ef48a1fc16b87040a240e00e0007c508dbd
rocco.malvaso <malvasorocco3@gmail.com>
2023-11-21 22:14:09 +0100

    Change method for compile command
    Now you need to put the command and code in the same line, removed the conversationHandler

--- callbacks/compile.py
+++ callbacks/compile.py
@@ -4,21 +4,12 @@ from telegram.ext import CallbackContext
 from utils.constant import payload
 from utils.utils import CompileResult, lines_output
 
-
-WAITING_FOR_REPLY = 1
-
 def compile(update: Update, context: CallbackContext) -> int:
-    user_name = update.message.from_user.first_name
-    text = "Siamo felici che tu voglia usare questo bot!\n".format(user_name)
-    text += "Come prossimo messaggio invia il codice da compilare in <b>Rust</b>"
-    update.message.reply_html(
-        text, reply_to_message_id=update.message.message_id)
-    return  WAITING_FOR_REPLY
-
-def fetch_reply(update: Update, context: CallbackContext):
-    text = "Stai compilando questo codice:\n"
+    user_input = ' '.join(context.args)
+    text = "Stai compilando questo codice:\n" + user_input
     update.message.reply_html(text)
     result = compiler(update.message.text)
+    update.message.reply_html(result)
 
 
 def compiler(code : str) -> str:
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from telegram.ext import CommandHandler, Updater, CallbackContext, MessageHandle
 import logging
 from utils.constant import token
 from callbacks.start import start
-from callbacks.compile import fetch_reply, WAITING_FOR_REPLY
 from callbacks.decode import decode
 from callbacks.compile import compile
 
@@ -16,12 +15,14 @@ if __name__ == '__main__':
     logger.info("Bot starting...")
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('compile', compile)],
-        states={WAITING_FOR_REPLY: [MessageHandler(Filters.text & ~Filters.command, fetch_reply)]},
-        fallbacks=[]
-    )
-    dispatcher.add_handler(conv_handler)
+    # conv_handler = ConversationHandler(
+    #     entry_points=[CommandHandler('compile', compile)],
+    #     states={WAITING_FOR_REPLY: [MessageHandler(Filters.text & ~Filters.command, fetch_reply)]},
+    #     fallbacks=[]
+    # )
+    # dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(CommandHandler(
+        'compile', compile, filters=Filters.text))
     dispatcher.add_handler(CommandHandler(
         'start', start, filters=Filters.text))
     dispatcher.add_handler(CommandHandler(
