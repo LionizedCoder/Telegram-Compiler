62a8cbbd0abc40dca48daf4270cc5ae73423bc52
rocco.malvaso <malvasorocco3@gmail.com>
2023-11-22 22:26:44 +0100

    fix formatter function
    fix formatter function removing json.dumps and join array of context.arg with space

--- callbacks/formatter.py
+++ callbacks/formatter.py
@@ -12,15 +12,13 @@ def formatted(update: Update, context: CallbackContext) -> None:
     user_input = ' '.join(context.args)
     if user_input != " ":
         formatted_payload['source'] = f'{user_input}'
-        logger.debug(formatted_payload['source'])
-        logger.debug(formatted_payload)
         r = requests.post("https://godbolt.org/api/format/rustfmt", json = formatted_payload, headers= headers)
         try:
             data = r.json()
+            data['answer'] = data['answer'].encode().decode('unicode-escape')
+            formatted_content = f"```rust\n{data['answer']}\n```"
+            update.message.reply_text(formatted_content, parse_mode='Markdown')
         except Exception as e:
-            logger.error(e)
-        data['answer'] = data['answer'].encode().decode('unicode-escape')
-        formatted_content = f"```rust\n{data['answer']}\n```"
-        update.message.reply_text(formatted_content, parse_mode='Markdown')
+            update.message.reply_text("C'è stato un errore con la tua richiesta, riprova più tardi.")
     else:
         update.message.reply_html("Non hai inserito nessun codice da formattare")
