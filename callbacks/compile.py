49f11e5cb4f17353cd88742339a05e2b8c47e24f
rocco.malvaso <malvasorocco3@gmail.com>
2023-11-23 19:02:08 +0100

    Add error when code not compile
    add error output when code not compile with format api

--- callbacks/compile.py
+++ callbacks/compile.py
@@ -14,11 +14,16 @@ def compile(update: Update, context: CallbackContext) -> None:
         if result['code'] == 0: 
             output = result['stdout'][0]['text']
             if output:
-                update.message.reply_text(f"✅ L'Output del tuo codice è:\n```{output}```", parse_mode='Markdown')
+                update.message.reply_text(f"✅ L'Output del tuo codice è:\n```\n{output}```", parse_mode='Markdown')
             else:
                 update.message.reply_text(f"✅ Il tuo codice ha compilato correttamente ma non ha prodotto un output!")
         else:
-            update.message.reply_text("❌ Errore di compilazione... La gestione dell'errore verrà implementata a breve")
+            update.message.reply_text("❌ Errore di compilazione...")
+            req = requests.post("https://godbolt.org/api/format/rustfmt", json = payload, headers={'Accept': 'application/json'})
+            data = req.json()
+            data['answer'] = data['answer'].encode().decode('unicode-escape')
+            formatted_content = f"```\n{data['answer']}\n```"
+            update.message.reply_text(formatted_content, parse_mode='Markdown')
     else:
         update.message.reply_html("Non hai inserito nessun codice da compilare")
 
